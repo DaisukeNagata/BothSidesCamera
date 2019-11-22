316162b31dd8cd08249a4bd5fe76af0bf25209cf
永田大祐 <dbank0208@gmail.com>
2019-11-22 16:12:52 +0900

    Pod Version 0.1.2

--- Example/BothSidesCamera/ViewController.swift
+++ Example/BothSidesCamera/ViewController.swift
@@ -12,28 +12,32 @@ import BothSidesCamera
 class ViewController: UIViewController {
 
     private var previewView: BothSidesView?
-    
-    var btAction = UIButton ()
+    lazy var  btn: UIButton = {
+        let btn = UIButton()
+        btn.setImage(UIImage(named: ""), for: .normal)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.layer.cornerRadius = 15
+        btn.backgroundColor = .red
+        return btn
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //回転の検知
+
         UIDevice.current.beginGeneratingDeviceOrientationNotifications()
-        
-        // 画面ロック
+
         UIApplication.shared.isIdleTimerDisabled = true
 
         previewView = BothSidesView(frame: view.frame)
         previewView?.setSessionPreset(state: .hd1920x1080)
         view.addSubview(previewView!)
-        view.addSubview(btAction)
-
-        btAction.frame.size = CGSize(width: 100, height: 100)
-        btAction.frame.origin.y = 100
-        btAction.backgroundColor = .red
-        btAction.addTarget(self, action: #selector(aaaa), for: .touchUpInside)
+        view.addSubview(btn)
+        btn.addTarget(self, action: #selector(btaction), for: .touchUpInside)
         
+        self.tabBarController?.tabBar.addSubview(btn)
+        self.tabBarController?.tabBar.centerXAnchor.constraint(equalTo: btn.centerXAnchor).isActive = true
+        self.tabBarController?.tabBar.topAnchor.constraint(equalTo: btn.centerYAnchor).isActive = true
+
         NotificationCenter.default.addObserver( self,
                                                 selector:#selector(background),
                                                 name: UIApplication.didEnterBackgroundNotification,object: nil)
@@ -43,31 +47,37 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        // PreviewView Size
         guard let pre = previewView else { return }
         pre.frontCameraVideoPreviewView.transform = pre.frontCameraVideoPreviewView.transform.scaledBy(x: 0.5, y: 0.5)
     }
 
     @objc func background() {
         print("background")
+        // stop camera
         previewView!.stopRunning()
     }
     
     @objc func foreground() {
         print("foreground")
+        // start camera
         previewView?.cmaeraStart(flg: flg, completion: saveBtn)
     }
 
     var flg = false
-    @objc func aaaa() {
+    @objc func btaction() {
+        // // start camera
         previewView?.cmaeraStart(flg: flg, completion: saveBtn)
         if flg == false {
+            self.tabBarController?.tabBar.backgroundColor = .red
             flg = true
         } else {
+            self.tabBarController?.tabBar.backgroundColor = .gray
             flg = false
         }
     }
 
     func saveBtn() {
-        print("1111")
+        print("movie save")
     }
 }
